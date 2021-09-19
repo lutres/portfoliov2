5c250cff8aaaf0b5a9509c2b8ae14bcc377b49e1
Luis <luismoreno785@gmail.com>
2021-09-19 10:32:36 -0400

    bg animation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './styles.css';
+// import './css/all.css';
 
 const categoriesBtn = document.querySelectorAll('.categories-btn')
 
@@ -89,11 +90,11 @@ async function handleSubmit(event) {
 const bgDiv = document.querySelector('.bg');
 
 for (let i = 0; i < window.innerHeight; i++) {
-
+    
     let randomWidth = Math.random() * window.innerWidth;
     let randomHeight = Math.random() * window.innerHeight;
     
-    const star = document.createElement('div');
+    let star = document.createElement('div');
 
     star.classList.add('star');
     bgDiv.appendChild(star);
@@ -101,8 +102,18 @@ for (let i = 0; i < window.innerHeight; i++) {
     star.style.left = `${Math.floor(randomWidth)}px`;
     star.style.top = `${Math.floor(randomHeight)}px`;
 
-    let randomTransitionY = Math.random() * window.innerHeight;
-    let randomTransitionX = Math.random() * window.innerWidth;
+}
+
+// Activar animación de fondo
+
+const nightModeBtn = document.querySelector('#animation');
+let star = document.querySelectorAll('.star');
+let play = false;
+let starAnimation = [];
+
+for (let i = 0; i < star.length; i++) {
+            
+    let randomTransition = Math.random() * window.innerHeight;
 
     let axis = Math.floor(Math.random() * 3);
     let sign = Math.floor(Math.random() * 2);
@@ -110,15 +121,57 @@ for (let i = 0; i < window.innerHeight; i++) {
     axis === 0 ? axis = 'Y' : axis = 'X';
     sign === 0 ? sign = '-' : sign = '';
 
-    let starKeyframeY = [
+    let starKeyframe = [
         {transform: `translate${axis}(0px)`},
-        {transform: `translate${axis}(${sign}${Math.floor(randomTransitionY)}px)`},
+        {transform: `translate${axis}(${sign}${Math.floor(randomTransition)}px)`},
         {transform: `translate${axis}(0px)`}
     ];
 
-    star.animate(starKeyframeY, {
+    starAnimation.push(star[i].animate(starKeyframe, {
         duration: 100000,
         iterations: Infinity
+    })); 
+
+    starAnimation.forEach(animation => {
+       animation.pause() 
     });
 
 }
+
+nightModeBtn.addEventListener('click', () => {
+    
+        if (play === false) {
+
+            starAnimation.forEach(animation => {
+                animation.play() 
+             });
+         
+        nightModeBtn.innerHTML = 
+        `
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+            <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>
+        `
+
+        nightModeBtn.title = 'Desactivar animación de fondo';
+
+        play = true;
+
+    } else {
+
+        starAnimation.forEach(animation => {
+            animation.pause() 
+         });
+
+        nightModeBtn.innerHTML =
+        `
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16">
+                <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+        </svg>
+        `
+
+        play = false;
+
+    }
+
+});
